dc53e4b5712127f57ded1adbf68fb0b065ce697e
ime <koussemonaurel@gmail.com>
2026-09-26 20:52:44 +0200

    feat(scrapers): add asterscans scraper

---
+++ scrapers/webtoons/definitions/webtoons/en/asterscans.ts
@@ -0,0 +1,4 @@
+import { MangaThemesiaScraper } from '../../../engine/mangathemesia';
+export class AsterscansScraper extends MangaThemesiaScraper {
+  constructor() { super('Aster Scans', 'https://asterscans.com', 'en'); }
+}
